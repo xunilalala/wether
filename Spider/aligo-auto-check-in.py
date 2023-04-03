d70f18a48c8db56a411e97e49da8b5636c8d805b
旭霓 <64685627+xunilalala@users.noreply.github.com>
2023-04-03 21:34:17 +0800

    Update aligo-auto-check-in.py

--- Spider/aligo-auto-check-in.py
+++ Spider/aligo-auto-check-in.py
@@ -1,11 +1,20 @@
 from aligo import Aligo
 
+
+# 方法1
+# if __name__ == '__main__':
+#     ali = Aligo()
+#     r = ali._post('/v1/activity/sign_in_list', host='https://member.aliyundrive.com', body={})
+#     print(r.status_code)
+
+# 方法2
 class CAligo(Aligo):
     def sign_in_list(self):
         return self._post('/v1/activity/sign_in_list', host='https://member.aliyundrive.com', body={})
 
+
 if __name__ == '__main__':
-    ali = CAligo(email=('3120294679@qq.com', 'github远程签到用'))
+    ali = CAligo()
     r = ali.sign_in_list()
     result = r.json()['result']
     signInCount = result['signInCount']
