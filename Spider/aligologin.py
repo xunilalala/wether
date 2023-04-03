cfcd7e228d995ad590a95d2feb80dc3c732bf8bd
旭霓 <64685627+xunilalala@users.noreply.github.com>
2023-04-03 21:42:07 +0800

    Update aligologin.py

--- Spider/aligologin.py
+++ Spider/aligologin.py
@@ -1,4 +1,8 @@
 from aligo import Aligo
 import time
-ali = Aligo(email=('3120294679@qq.com', 'github_login'))
+if __name__ == '__main__':
+    # 详情请查看源码中的文档字符串
+    ali = Aligo(email=('3120294679@qq.com', 'GITHUB'))
+    user = ali.get_user()
+    print(user.user_id, user.user_name, user.nick_name)
 time.sleep(30)
