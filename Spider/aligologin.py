f08cf86cc0f1a1e03158fe49578beb27cac2f98e
旭霓 <64685627+xunilalala@users.noreply.github.com>
2023-04-06 08:58:58 +0800

    Delete aligologin.py

--- Spider/aligologin.py
+++
@@ -1,8 +0,0 @@
-from aligo import Aligo
-import time
-if __name__ == '__main__':
-    # 详情请查看源码中的文档字符串
-    ali = Aligo(email=('3120294679@qq.com', 'GITHUB'))
-    user = ali.get_user()
-    print(user.user_id, user.user_name, user.nick_name)
-time.sleep(30)
